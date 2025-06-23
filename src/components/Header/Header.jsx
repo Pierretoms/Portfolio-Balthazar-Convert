2426581a84916dae880f739bc82952d0db707201
Pierretoms <pierre.toms13@gmail.com>
2025-06-23 12:54:07 +0200

    suppression du webp pour du avif & maj du responsive avec ajout d'un menu déroulant pour le header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./Header.scss"
 
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
         <nav>
             <h1>Balthazar Convert</h1>
-            <ul>
+
+            <button className="burger" onClick={toggleMenu} aria-label="Menu mobile">
+              <i className={`fa-solid ${menuOpen ? 'fa-xmark' : 'fa-bars'}`}></i>
+            </button>
+
+            <ul className={menuOpen ? "open" : ""}>
                 <li><a href='#about'>A propos</a></li>
                 <li><a href='#gallery'>Galerie</a></li>
-                <li><a href='#contact'>Contact</a></li>
+                <li><a href="mailto:convert.balthazar@gmail.com" aria-label="me contacter par mail">Contact</a></li>
                 <li><a href="https://www.instagram.com/balthazar_convert/" target="_blank" className="social-link" aria-label="mon compte instagram">
                 <i className="fa-brands fa-instagram"></i></a></li>
             </ul>
